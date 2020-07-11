97d15491b42847d9119bcff6791eef7d11f5298d
Zaccari S <zacosx@gmail.com>
2020-07-11 04:53:07 -0400

    Changing this to just support 1 image at a time...

--- Sources/Twilio/Models/OutgoingMMS.swift
+++ Sources/Twilio/Models/OutgoingMMS.swift
@@ -5,28 +5,18 @@ public struct OutgoingMMS: OutgoingMessage {
     let from: String
     let to: String
     
-    /// Up to 10 Media URLs are supported.
-    let mediaURL: [URL]
+    let mediaURL: URL
     
     public init(body: String? = nil, from: String, to: String, mediaURL: URL) {
         self.body = body
         self.from = from
         self.to = to
         
-        self.mediaURL = [mediaURL]
+        self.mediaURL = mediaURL
     }
     
-    public init(body: String? = nil, from: String, to: String, mediaURLs: [URL]) {
-        self.body = body
-        self.from = from
-        self.to = to
-        
-        self.mediaURL = mediaURLs
-    }
-
     private enum CodingKeys : String, CodingKey {
         case body = "Body", from = "From", to = "To", mediaURL = "MediaUrl"
     }
-    
     
 }
